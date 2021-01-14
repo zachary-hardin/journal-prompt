4e009bcd018fd13a3eff575584095b8c9566fded
Zachary Hardin <zbhardin@gmail.com>
2021-01-14 15:59:07 -0600

    Clean up PromptService

--- journal-prompt-app/src/services/PromptService.js
+++ journal-prompt-app/src/services/PromptService.js
@@ -1,13 +1,11 @@
 import * as axios from 'axios';
 
+const BASE_URL = 'https://thawing-fortress-26537.herokuapp.com';
+
 export const fetchPrompt = () => {
-  return axios.get('https://thawing-fortress-26537.herokuapp.com/prompt');
+  return axios.get(`${BASE_URL}/prompt`);
 }
 
 export const insertPrompt = (prompt) => {
-  return axios.post('https://thawing-fortress-26537.herokuapp.com/new-prompt', null, {
-    params: {
-      prompt
-    }
-  });
+  return axios.post(`${BASE_URL}/new-prompt`, null, { params: { prompt } });
 }
